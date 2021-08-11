e982ed89ad7d84850d34dc9a6f0fa63eed2a441f
Vinh <tranquangvinh5899@gmail.co>
2021-08-11 20:52:51 +0700

    test

--- wp-content/themes/vpba/actions/footer/index.php
+++ wp-content/themes/vpba/actions/footer/index.php
@@ -7,11 +7,11 @@ function action_advertisements_footer(){
     $advertisements = json_decode($advertisements);
     render_marquee_sidebar_advertisement($advertisements, "left");
 }
-
+    
 function action_menu_footer(){
     render_menu_footer(get_theme_mod('menu_id_footer'));
 }
-
+    
 function action_list_social(){
     render_list_social(json_decode(get_theme_mod('item_social')));
 }
