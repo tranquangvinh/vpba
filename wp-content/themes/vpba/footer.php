4d966944eb38a9fd078f855cfcd52b002911bdf2
longhalan <longnguyen476@gmail.com>
2021-08-18 23:43:15 +0700

    Css footer
    CSS footer

--- wp-content/themes/vpba/footer.php
+++ wp-content/themes/vpba/footer.php
@@ -4,8 +4,16 @@
         <div class="contact">
            <?php echo wp_nav_menu(['menu' => 'menu-footer-1']); ?>
         </div>
-        <div class="contact">
+         <div class="contact">
            <?php echo wp_nav_menu(['menu' => 'menu-footer-2']); ?>
+        </div>
+        <div class="footer_info">
+            <p style="color: #172973;"><b>Copyright © 2009 - 2021 Đạo Phật Ngày Nay</b><br>
+                Biên tập: <b>Thượng tọa Thích Nhật Từ</b> | Phụ trách mạng: <b>Thích Ngộ Dũng - Hải Hạnh - Giác Định</b><br>
+                <b>Bài vở đóng góp, xin gởi attachment về Ban biên tập qua địa chỉ email: goibaidaophatngaynay@yahoo.com hoặc thichngodung@gmail.com</b></p>
+            <p>Liên lạc thư tín với Tỳ-kheo Thích Nhật Từ xin gởi về: <br>
+                Chùa Giác Ngộ, 92 Nguyễn Chí Thanh, phường 3, quận 10, TP. Hồ Chí Minh, Việt Nam<br>
+                Điện thoại từ nước ngoài: +84-908-153-160 (M); +84-8-830-9570 (H); trong nước: 0908153160; 8309570.</p>
         </div>
     </div>
 </div>
