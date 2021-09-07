596aaf88855516b48237fb1a3764f85fa8c8701e
trungvh06 <88734497+trungvh06@users.noreply.github.com>
2021-09-07 22:38:43 +0700

    update link url sidebar

--- wp-content/themes/vpba/actions/sidebar/render/slider.php
+++ wp-content/themes/vpba/actions/sidebar/render/slider.php
@@ -1,16 +1,17 @@
 <?php 
-function render_slider($slider){
-    ?>
+function render_slider($slider){?>
     <div class="link_slider">
-        <?php foreach ( $slider as $image){ ?>
-            <?php 
-                 $arr = explode(",",$image->link);
-                 $url = substr($arr[0],13,strlen($arr[0])-1);?>
-            <a href="<?php echo $url?>">
+        <?php foreach ( $slider as $image){ 
+            $link = "";
+            $link_external = "";?>
+            <?php if($image->link){ 
+                $link = json_decode($image->link);
+                $link_external = $link->external;
+            }?>
+            <a href="<?php echo $link_external?>">
                 <img src="<?php echo $image->image?>" alt="">
             </a>
-        <?php
-    } ?>
+        <?php } ?>
     </div>
 <?php
 }
