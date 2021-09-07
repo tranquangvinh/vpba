e25d4e51421a3558fbeb4c2f3d5bb891f23d2f30
trungvh06 <88734497+trungvh06@users.noreply.github.com>
2021-09-07 21:19:59 +0700

    fixbug Url Sidebar

--- wp-content/themes/vpba/actions/sidebar/render/slider.php
+++ wp-content/themes/vpba/actions/sidebar/render/slider.php
@@ -3,7 +3,10 @@ function render_slider($slider){
     ?>
     <div class="link_slider">
         <?php foreach ( $slider as $image){ ?>
-            <a href="<?php echo $image->link ?>">
+            <?php 
+                 $arr = explode(",",$image->link);
+                 $url = substr($arr[0],13,strlen($arr[0])-1);?>
+            <a href="<?php echo $url?>">
                 <img src="<?php echo $image->image?>" alt="">
             </a>
         <?php
@@ -11,9 +14,3 @@ function render_slider($slider){
     </div>
 <?php
 }
-
-
-
-
-
-
