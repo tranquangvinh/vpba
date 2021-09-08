4f5e49a7f899d8ca48fa38e9999cc5b79e59cdea
longhalan <longnguyen476@gmail.com>
2021-09-08 22:16:12 +0700

    delete sidebar in post story detail
    delete sidebar in post story detail

--- wp-content/themes/vpba/single-story.php
+++ wp-content/themes/vpba/single-story.php
@@ -10,7 +10,7 @@ if(is_array($terms) && count($terms) > 0){
 
 <div class="container">
 	<div class="row">
-		<div class="col-md-8 col-sm-12">
+		<div class="col-md-12 col-sm-12">
 			<?php get_template_part('template-parts/story/detail', 'detail', ['term_id' => $terms[0]->term_id, 'post_id' => get_the_ID()]); ?>
 
 			<!-- related story -->
@@ -19,18 +19,6 @@ if(is_array($terms) && count($terms) > 0){
                     get_template_part('template-parts/story/related', 'realted', ['term_id' => $terms[0]->term_id]);
                 }
             ?>
-		</div>
-		<div class="col-md-4 col-sm-12" style="margin-top: 50px">
-			<?php get_sidebar(); ?>
-			<?php get_template_part(
-				'template-parts/fanpage/index', 
-				'fanpage', 
-				[
-				'link_fanpage' => get_theme_mod('link_fanpage_info'),
-				'width' => 300,
-				'height' => 500
-				]
-			); ?>
 		</div>
 	</div>
 </div>
