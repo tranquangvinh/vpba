9fb703eddc2788c5ebbe86861674ee1f6bf03144
TuanVo <anhtuanadmin@gmail.com>
2021-09-12 23:24:26 +0700

    UPDATE.

--- wp-content/themes/vpba/template-parts/post/detail.php
+++ wp-content/themes/vpba/template-parts/post/detail.php
@@ -3,11 +3,11 @@ while ( have_posts() ) :
 	the_post(); ?>	
 	<div class="post-detail">
 		<h1 class="title"><?php echo get_the_title(); ?></h1>
-		<div class="time-create-date my-2" id="time-create-date"></div>
-		<div id="time-create-date-hide" class="d-none"><?php echo get_post_time( 'l F j, Y'); ?></div>
 		<div class="content">
 			<?php echo get_the_content(); ?>
 		</div>
+		<div class="time-create-date my-2" id="time-create-date"></div>
+		<div id="time-create-date-hide" class="d-none"><?php echo get_post_time( 'l F j, Y'); ?></div>
 	</div>
 	<div class="tag-post">
 		<?php 
